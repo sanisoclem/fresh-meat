d9cfde6476b71a58ad3177ee2364b0065afa7c1e
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2022-07-17 23:22:30 +1000

    cleanup

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,20 +3,18 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/auth/github.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/ledger/[id].tsx";
-import * as $5 from "./routes/login.tsx";
+import * as $1 from "./routes/auth/github.tsx";
+import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/ledger/[id].tsx";
+import * as $4 from "./routes/login.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/auth/github.tsx": $2,
-    "./routes/index.tsx": $3,
-    "./routes/ledger/[id].tsx": $4,
-    "./routes/login.tsx": $5,
+    "./routes/auth/github.tsx": $1,
+    "./routes/index.tsx": $2,
+    "./routes/ledger/[id].tsx": $3,
+    "./routes/login.tsx": $4,
   },
   islands: {},
   baseUrl: import.meta.url,
